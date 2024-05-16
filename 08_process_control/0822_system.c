22983589e90ceeb942ecd68be711d931963d7606
szyhdev <szyuanhua@163.com>
2024-05-16 11:53:09 +0800

    update chapter 08 source code

--- 08_process_control/0822_system.c
+++ 08_process_control/0822_system.c
@@ -8,14 +8,14 @@ int system(const char *cmdstring)  /* version without signal handling */
     int status;
 
     if (cmdstring == NULL) {
-        return(1);  /* always a command processor with UNIX */
+        return(1);            /* always a command processor with UNIX */
     }
 
     if ((pid = fork()) < 0) {
-        status = -1;  /* probably out of processes */
+        status = -1;          /* probably out of processes */
     } else if (pid == 0) {  /* child */
         execl("/bin/sh", "sh", "-c", cmdstring, (char *)0);
-        _exit(127);  /* execl error */
+        _exit(127);           /* execl error */
     } else {                /* parent */
         while (waitpid(pid, &status, 0) < 0) {
             if (errno != EINTR) {
